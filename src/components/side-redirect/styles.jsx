82e746236400f02d5374abaa40ed3cdc3e494613
[Rafael Assad] <[rafael.assad1010@gmail.com]>
2021-01-12 18:02:42 -0300

    Feat: ajustado o estilo da página semelhante ao do figma. ajustes no input do material-ui, tentativa de deixar parecido com o figma, alguns ajustes menores

---
+++ src/components/side-redirect/styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const SideRedirectContainer = styled.span`
+  margin-top:10px;
+  color: #fff;
+  a{
+    color: green;
+    font-weight:700;
+    text-decoration:none
+  }
+`
