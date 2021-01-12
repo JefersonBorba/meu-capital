9ce7e92150bbbfbffba40bfd782d43b3e4337f84
Felipe Souza <felipesouzadsgn@gmail.com>
2021-01-12 17:42:35 -0300

    styles: adicionado estilizaçao do component resume-goal

---
+++ src/components/dashboard-resume-goal/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 2px solid #494984;
+  width: 200px;
+  border-radius: 10px;
+  margin: 20px 0;
+`;
+export const Content = styled.div`
+  padding: 15px;
+  h1 {
+    margin-bottom: 10px;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
