6d828826472a7b85521072f3cada81f640d41fb0
Jeferson Borba <jeff@pop-os.localdomain>
2021-01-13 14:32:51 -0300

    adicionado linechart ao dashboard

---
+++ src/components/dashboard-linechart/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    h3{
+        color: white;
+        margin: 0 0 3% 3%;
+    }
+`;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,21 +3,24 @@ import ResumoGastos from "../../components/dashboard-resumo-gastos";
 import SideMenu from "../../components/dashboard-sidemenu";
 import ResumeGoal from "../../components/dashboard-resume-goal";
 import ResumeTransaction from "../../components/dashboard-resume-transaction";
-import { Container, RightContainer, Breakable } from "./style"
+import DashboardLineChart from "../../components/dashboard-linechart"
+import { Container, RightContainer, Breakable, Graphs } from "./style"
 const Dashboard = () => (
   <>
     <Header title="Dashboard" />
     <Container>
       <SideMenu />
       <Breakable>
-        <ResumoGastos/>
+        <Graphs>
+          <ResumoGastos/>
+          <DashboardLineChart />
+        </Graphs>
         <RightContainer>
           <ResumeGoal />
           <ResumeTransaction />
         </RightContainer>
       </Breakable>
       </Container>
-    
   </>
 );
 export default Dashboard;
