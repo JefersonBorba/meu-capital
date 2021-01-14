75121ecea6ccac0ef5f3f5f3c22098dc2eb5ebd6
Felipe Souza <felipesouzadsgn@gmail.com>
2021-01-14 10:59:34 -0300

    styles: atualizando tamanho do PizzaChart

--- src/components/pizzachart/index.jsx
+++ src/components/pizzachart/index.jsx
@@ -1,18 +1,16 @@
-import {
-  PieChart, Pie, Legend, Tooltip,
-} from 'recharts';
+import { PieChart, Pie, Legend, Tooltip } from "recharts";
 const PizzaChart = () => {
-    const data01 = [
-        { name: 'Gastos', value: 400, fill:"#e786d7" },
-        { name: 'Ganhos', value: 1100, fill:"#7f7fd5" }
-      ];
-      return (
-        <PieChart width={300} height={300}>
-            <Legend verticalAlign="top" height={36}/>
-          <Pie dataKey="value" data={data01}  outerRadius={80} label />
-          <Tooltip />
-        </PieChart>
-      );
-}
+  const data01 = [
+    { name: "Gastos", value: 400, fill: "#e786d7" },
+    { name: "Ganhos", value: 1100, fill: "#7f7fd5" },
+  ];
+  return (
+    <PieChart width={200} height={200}>
+      <Legend verticalAlign="top" height={36} />
+      <Pie dataKey="value" data={data01} outerRadius={80} label />
+      <Tooltip />
+    </PieChart>
+  );
+};
 
 export default PizzaChart;
