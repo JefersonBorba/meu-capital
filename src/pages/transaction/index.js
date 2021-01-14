dfdc5caedd11faa3447eae52201df2fa89499b29
Felipe Souza <felipesouzadsgn@gmail.com>
2021-01-14 10:55:43 -0300

    atualizando componente transaction

--- src/pages/transaction/index.js
+++ src/pages/transaction/index.js
@@ -1,9 +1,12 @@
 import Header from "../../components/header";
+
 import SideMenu from "../../components/dashboard-sidemenu";
 import ResumeGoal from "../../components/dashboard-resume-goal";
-import ResumeBalance from "../../components/transaction-resume-balance";
 
+import ResumeBalance from "../../components/transaction-resume-balance";
 import ContentTransaction from "../../components/transaction-content";
+
+import PizzaChart from "../../components/pizzachart";
 
 import { Container, RightContainer, Breakable } from "./styles";
 
@@ -17,6 +20,7 @@ const Transaction = () => (
         <RightContainer>
           <ResumeGoal />
           <ResumeBalance />
+          <PizzaChart />
         </RightContainer>
       </Breakable>
     </Container>
