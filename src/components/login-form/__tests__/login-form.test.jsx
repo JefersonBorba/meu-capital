214cf2df1008f147f19d472b2edf6d0e1141961b
[Rafael Assad] <[rafael.assad1010@gmail.com]>
2021-01-19 08:00:22 -0300

    testes de formulário de login concluido, trabalhando no register

--- src/components/login-form/__tests__/login-form.test.jsx
+++ src/components/login-form/__tests__/login-form.test.jsx
@@ -1,26 +1,27 @@
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
-// import {toBeInTheDocument } from '@testing-library/jest-dom'
 
 import { Provider } from "react-redux";
 import store from "../../../store";
 import LoginForm from "../index";
 
 describe("Testing Login Form", () => {
-  test("Should email be valid", async () => {
+  test("Should email be valid", () => {
     render(
       <Provider store={store}>
         <LoginForm />
       </Provider>
     );
+    
     userEvent.type(
       screen.getByPlaceholderText("Digite seu email"),
-      "exemplo@email.com"
+      "exemplo@gmail.com"
     );
-    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123456");
+    userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "123456");
     userEvent.click(screen.getByRole("button"));
-    const message = await screen.findByText(/E-mail inválido!/);
-    expect(message).not.toBeInTheDocument();
+    const message = screen.queryByText(/E-mail inválido!/);
+    
+    expect(message).toBeFalsy();
   });
 
   test("Should email be invalid", async () => {
@@ -35,43 +36,67 @@ describe("Testing Login Form", () => {
     );
     userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123456");
     userEvent.click(screen.getByRole("button"));
-    const message = await screen.findByText(/E-mail inválido!/);
-    expect(message).toBeInTheDocument();
+    const message =  await screen.findByText(/E-mail inválido!/);
+
+    expect(message).toBeTruthy();
   });
 
-  // test("Should password be valid", async ()=> {
-  //   render(<LoginForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@email.com")
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123456")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findByText(/Campo obrigatório!/);
-  //   expect(message).not.toBeInTheDocument()
-  // })
+  test("Should password be valid", ()=> {
+    render(
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@email.com")
+    userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "123456")
+    userEvent.click(screen.getByRole("button"))
 
-  // test("Should password be invalid", async ()=> {
-  //   render(<LoginForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@email.com")
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findByText(/Campo obrigatório!/);
-  //   expect(message).toBeInTheDocument()
-  // })
+    const message = screen.queryByText(/Campo obrigatório!/);
 
-  // test("Should email and password be required", async ()=> {
-  //   render(<LoginForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "")
-  //   userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findAllByAltText(/Campo obrigatório!/);
-  //   expect(message).toHaveLength(2)
-  // })
+    expect(message).toBeFalsy()
+  })
 
-  // test("Should email and password be validated", async ()=> {
-  //   render(<LoginForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemple@email.com")
-  //   userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "123456")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findByText(/Campo obrigatório!/);
-  //   expect(message).not.toBeInTheDocument()
-  // })
+  test("Should password be invalid", async ()=> {
+    render(
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@email.com")
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123")
+    userEvent.click(screen.getByRole("button"))
+
+    const message = await screen.findByText(/Campo obrigatório!/);
+
+    expect(message).toBeTruthy()
+  })
+
+  test("Should email and password be required", async ()=> {
+    render(
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "")
+    userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "")
+    userEvent.click(screen.getByRole("button"))
+
+    const message =  await screen.findAllByText(/Campo obrigatório!/);
+
+    expect(message).toHaveLength(2)
+  })
+
+  test("Should email and password be validated",  ()=> {
+    render(
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemple@email.com")
+    userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "123456")
+    userEvent.click(screen.getByRole("button"))
+    const message =  screen.queryByText(/Campo obrigatório!/);
+
+    expect(message).toBeFalsy()
+  })
 });
--- src/components/register-form/__tests__/register-form.test.jsx
+++ src/components/register-form/__tests__/register-form.test.jsx
@@ -1,26 +1,27 @@
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
-// import {toBeInTheDocument } from '@testing-library/jest-dom'
 
 import { Provider } from "react-redux";
 import store from "../../../store";
 import RegisterForm from "../index";
 
 describe("Testing Login Form", () => {
-  test("Should email be valid", async () => {
+  test("Should email be valid", () => {
     render(
       <Provider store={store}>
         <RegisterForm />
       </Provider>
     );
-    userEvent.type(
-      screen.getByPlaceholderText("Digite seu email"),
-      "exemplo@email.com"
-    );
-    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123456");
+    
+    userEvent.type(screen.getByPlaceholderText("Digite seu nome"), "Ines Zistente");
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@gmail.com");
+    userEvent.type(screen.getByPlaceholderText("Digite sua senha"), "123456");
+    userEvent.type(screen.getByPlaceholderText("Confirme sua senha"), "123456");
     userEvent.click(screen.getByRole("button"));
-    const message = await screen.findByText(/E-mail inválido!/);
-    expect(message).not.toBeInTheDocument();
+
+    const message = screen.queryByText(/E-mail inválido!/);
+    
+    expect(message).toBeFalsy();
   });
 
   test("Should email be invalid", async () => {
@@ -29,49 +30,74 @@ describe("Testing Login Form", () => {
         <RegisterForm />
       </Provider>
     );
-    userEvent.type(
-      screen.getByPlaceholderText("Digite seu email"),
-      "Outa coisa"
-    );
-    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123456");
+    userEvent.type(screen.getByPlaceholderText("Digite seu nome"), "Ines Zistente");
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "outra coisa");
+    userEvent.type(screen.getByPlaceholderText("Digite sua senha"), "123456");
+    userEvent.type(screen.getByPlaceholderText("Confirme sua senha"), "123456");
     userEvent.click(screen.getByRole("button"));
-    const message = await screen.findByText(/E-mail inválido!/);
-    expect(message).toBeInTheDocument();
+    const message =  await screen.findByText(/E-mail inválido!/);
+
+    expect(message).toBeTruthy();
   });
 
-  // test("Should password be valid", async ()=> {
-  //   render(<RegisterForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@email.com")
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123456")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findByText(/Campo obrigatório!/);
-  //   expect(message).not.toBeInTheDocument()
-  // })
+  test("Should password be valid", ()=> {
+    render(
+      <Provider store={store}>
+        <RegisterForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu nome"), "Ines Zistente");
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@gmail.com");
+    userEvent.type(screen.getByPlaceholderText("Digite sua senha"), "123456");
+    userEvent.type(screen.getByPlaceholderText("Confirme sua senha"), "123456");
+    userEvent.click(screen.getByRole("button"))
 
-  // test("Should password be invalid", async ()=> {
-  //   render(<RegisterForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@email.com")
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findByText(/Campo obrigatório!/);
-  //   expect(message).toBeInTheDocument()
-  // })
+    const message = screen.queryByText(/Campo obrigatório!/);
 
-  // test("Should email and password be required", async ()=> {
-  //   render(<RegisterForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "")
-  //   userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findAllByAltText(/Campo obrigatório!/);
-  //   expect(message).toHaveLength(2)
-  // })
+    expect(message).toBeFalsy()
+  })
 
-  // test("Should email and password be validated", async ()=> {
-  //   render(<RegisterForm/>)
-  //   userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemple@email.com")
-  //   userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "123456")
-  //   userEvent.click(screen.getByRole("button"))
-  //   const message = await screen.findByText(/Campo obrigatório!/);
-  //   expect(message).not.toBeInTheDocument()
-  // })
+  test("Should password be invalid", async ()=> {
+    render(
+      <Provider store={store}>
+        <RegisterForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemplo@email.com")
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "123")
+    userEvent.click(screen.getByRole("button"))
+
+    const message = await screen.findByText(/Campo obrigatório!/);
+
+    expect(message).toBeTruthy()
+  })
+
+  test("Should email and password be required", async ()=> {
+    render(
+      <Provider store={store}>
+        <RegisterForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "")
+    userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "")
+    userEvent.click(screen.getByRole("button"))
+
+    const message =  await screen.findAllByText(/Campo obrigatório!/);
+
+    expect(message).toHaveLength(2)
+  })
+
+  test("Should email and password be validated",  ()=> {
+    render(
+      <Provider store={store}>
+        <RegisterForm />
+      </Provider>
+    )
+    userEvent.type(screen.getByPlaceholderText("Digite seu email"), "exemple@email.com")
+    userEvent.type(screen.getByPlaceholderText("Digite sua Senha"), "123456")
+    userEvent.click(screen.getByRole("button"))
+    const message =  screen.queryByText(/Campo obrigatório!/);
+
+    expect(message).toBeFalsy()
+  })
 });
